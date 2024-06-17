2fe7d2984019d45a2c6735376b3f40b31ec5edaa
オウ　シュシュ <wz975318642@163.com>
2024-06-17 17:39:06 +0900

    实战1~3

---
+++ 项目实战/user/common/footer.php
@@ -0,0 +1,3 @@
+<div id="footer">
+    <p>&copy; PHP Project 2024</p>
+</div>
---
+++ 项目实战/user/register.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>用户注册</title>
+    <link rel="stylesheet" href="../public/css/login.css">
+    <?php include "./common/head.php" ?>
+</head>
+<body>
+    <?php include "./common/header.php" ?>
+
+    <main class="main">
+        <div class="container">
+            <h2 class="site-title">登録画面</h2>
+            <form action="" id="submit_form">
+                <ul>
+                    <li class="line">
+                        <!-- 快速复制键：alt+shift+下箭头 -->
+                        <span class="title">
+                            ユーザー名：
+                        </span>
+                        <input type="text" name="username">
+                    </li>
+                    <li class="line">
+                        <span class="title">
+                            パスワード：
+                        </span>
+                        <input type="password" name="password">
+                    </li>
+                    <li class="line">
+                        <span class="title">
+                            パスワード確認：
+                        </span>
+                        <input type="password" name="re_password">
+                    </li>
+                    <li class="line submit_line">
+                        <input type="submit" name="提交注册">
+                        <a href="./login.php">ログインヘ</a>
+                    </li>
+                </ul>
+            </form>
+        </div>
+    </main>
+    
+    <?php include "./common/footer.php" ?>
+</body>
+</html>
